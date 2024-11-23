42e95c06581bf222bb55b854bcc09fb813dded99
KauanFortunato <kauanmatiasfortunato@gmail.com>
2024-11-23 23:01:37 +0000

    Login adcionado. Pagina de cadastro

---
+++ app/src/main/java/com/mordekai/poggtech/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.mordekai.poggtech;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
