5ae87853d8d38d0723bf1fe1e453ff77de2b3fcf
KauanFortunato <kauanmatiasfortunato@gmail.com>
2024-12-07 15:33:54 +0000

    Home ui atualizada

--- app/src/main/java/com/mordekai/poggtech/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/mordekai/poggtech/ui/fragments/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.mordekai.poggtech.ui.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +31,51 @@ public class HomeFragment extends Fragment {
         sharedPrefHelper = new SharedPrefHelper(requireContext());
         user = sharedPrefHelper.getUser();
 
+        iniciarComponentes(view);
+
+
         return view;
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void iniciarComponentes(View view) {
+        ImageButton perfilButton = view.findViewById(R.id.perfil);
+        ImageButton menuLateralButton = view.findViewById(R.id.menuLateral);
+
+        perfilButton.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    v.animate().scaleX(0.9f).scaleY(0.9f).setDuration(50).start();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    v.animate().scaleX(1f).scaleY(1f).setDuration(100).start();
+                    break;
+            }
+            return false;
+        });
+
+        menuLateralButton.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    v.animate().scaleX(0.9f).scaleY(0.9f).setDuration(50).start();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    v.animate().scaleX(1f).scaleY(1f).setDuration(100).start();
+                    break;
+            }
+            return false;
+        });
+
+        perfilButton.setOnClickListener(v -> {
+            UserAccountFragment userAccountFragment = new UserAccountFragment();
+            requireActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.containerFrame, userAccountFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
+    }
+
 }
