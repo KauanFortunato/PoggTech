98eb1569536efb858722cf99567827aca8ea6fcb
KauanFortunato <kauanmatiasfortunato@gmail.com>
2025-01-03 21:13:32 +0000

    Interface alterada

--- app/src/main/java/com/mordekai/poggtech/data/remote/RetrofitClient.java
+++ app/src/main/java/com/mordekai/poggtech/data/remote/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://10.0.2.2/PoggTech-APIs/routes/";
+    private static final String BASE_URL = "http://192.168.1.91/PoggTech-APIs/routes/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
