9705a51cd6825a30f6ce533f88c1f1e53f096041
KauanFortunato <kauanmatiasfortunato@gmail.com>
2025-02-18 18:33:06 +0000

    Melhorando a conexao com o dominio

--- app/src/main/java/com/mordekai/poggtech/data/remote/RetrofitClient.java
+++ app/src/main/java/com/mordekai/poggtech/data/remote/RetrofitClient.java
@@ -4,17 +4,33 @@ import android.util.Log;
 
 import com.mordekai.poggtech.utils.AppConfig;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
+
     public static Retrofit getRetrofitInstance() {
+        String username = "admin";
+        String password = "D7b@IRia";
+
         Log.d("RetrofitClient", "getRetrofitInstance: " + AppConfig.getBaseUrl());
+
         if (retrofit == null) {
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(new BasicAuthInterceptor(username, password));
+
+            String baseUrl = AppConfig.getBaseUrl();
+            if (baseUrl == null) {
+                Log.e("RetrofitClient", "O base URL não foi definido.");
+                return null;
+            }
+
             retrofit = new Retrofit.Builder()
-                    .baseUrl(AppConfig.getBaseUrl())
+                    .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
         return retrofit;
