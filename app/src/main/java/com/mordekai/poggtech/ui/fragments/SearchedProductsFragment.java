91fb1a4e34538dbea4aa35fb642252121b86536e
KauanFortunato <kauanmatiasfortunato@gmail.com>
2025-03-31 13:30:13 +0100

    Pesquisa de produtos adicionada

--- app/src/main/java/com/mordekai/poggtech/ui/fragments/SearchedProductsFragment.java
+++ app/src/main/java/com/mordekai/poggtech/ui/fragments/SearchedProductsFragment.java
@@ -1,19 +1,43 @@
 package com.mordekai.poggtech.ui.fragments;
 
+import com.mordekai.poggtech.data.adapter.ProductSearchedAdapter;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mordekai.poggtech.R;
+import com.mordekai.poggtech.data.callback.RepositoryCallback;
+import com.mordekai.poggtech.data.model.Product;
+import com.mordekai.poggtech.data.model.User;
+import com.mordekai.poggtech.data.remote.ApiProduct;
+import com.mordekai.poggtech.data.remote.ApiService;
+import com.mordekai.poggtech.data.remote.RetrofitClient;
+import com.mordekai.poggtech.domain.ProductManager;
+import com.mordekai.poggtech.utils.SharedPrefHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchedProductsFragment extends Fragment implements HeaderFragment.HeaderListener {
 
     private HeaderFragment headerFragment;
+    private ProductSearchedAdapter productSearchedAdapter;
+    private SharedPrefHelper sharedPrefHelper;
+    private User user;
+    private EditText searchProd;
+    private RecyclerView searchedProducts;
+    private ApiProduct apiProduct;
+    private ProductManager productManager;
 
     @Nullable
     @Override
@@ -21,7 +45,43 @@ public class SearchedProductsFragment extends Fragment implements HeaderFragment
         View view = inflater.inflate(R.layout.fragment_searched_products, container, false);
         headerFragment = (HeaderFragment) getChildFragmentManager().findFragmentById(R.id.headerContainer);
 
+
+        sharedPrefHelper = new SharedPrefHelper(requireContext());
+        user = sharedPrefHelper.getUser();
+
+        initializeComponents(view);
+
+        productSearchedAdapter = new ProductSearchedAdapter(new ArrayList<>(), user.getUserId());
+        searchedProducts.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        searchedProducts.setAdapter(productSearchedAdapter);
+
+        apiProduct = RetrofitClient.getRetrofitInstance().create(ApiProduct.class);
+        productManager = new ProductManager(apiProduct);
+
+
+        getSearchedProducts(searchProd.getText().toString());
+
         return view;
+    }
+
+    public void getSearchedProducts(String search) {
+        productManager.searchProducts(search, new RepositoryCallback<List<Product>>() {
+
+            @Override
+            public void onSuccess(List<Product> result) {
+                productSearchedAdapter.updateProducts(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+    }
+
+    private void initializeComponents(View view) {
+        searchedProducts = view.findViewById(R.id.searchedProducts);
+        searchProd = getActivity().findViewById(R.id.searchProd);
     }
 
     @Override
