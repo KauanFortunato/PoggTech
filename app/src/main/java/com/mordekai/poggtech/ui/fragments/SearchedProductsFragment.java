aac646924f9939af11591a0f4dc46bf1861378ad
KauanFortunato <kauanmatiasfortunato@gmail.com>
2025-04-02 18:15:40 +0100

    Filtro e funções do card de pesquisa adicionado

--- app/src/main/java/com/mordekai/poggtech/ui/fragments/SearchedProductsFragment.java
+++ app/src/main/java/com/mordekai/poggtech/ui/fragments/SearchedProductsFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -35,9 +36,13 @@ public class SearchedProductsFragment extends Fragment implements HeaderFragment
     private SharedPrefHelper sharedPrefHelper;
     private User user;
     private EditText searchProd;
+    private Switch poggersFilter;
     private RecyclerView searchedProducts;
     private ApiProduct apiProduct;
     private ProductManager productManager;
+    private List<Integer> favoriteIds = new ArrayList<>();
+    private List<Product> allProducts = new ArrayList<>();
+
 
     @Nullable
     @Override
@@ -48,14 +53,15 @@ public class SearchedProductsFragment extends Fragment implements HeaderFragment
         sharedPrefHelper = new SharedPrefHelper(requireContext());
         user = sharedPrefHelper.getUser();
 
-        initializeComponents(view);
-
-        productSearchedAdapter = new ProductSearchedAdapter(new ArrayList<>(), user.getUserId());
-        searchedProducts.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        searchedProducts.setAdapter(productSearchedAdapter);
-
         apiProduct = RetrofitClient.getRetrofitInstance().create(ApiProduct.class);
         productManager = new ProductManager(apiProduct);
+
+        initializeComponents(view);
+
+        productSearchedAdapter = new ProductSearchedAdapter(new ArrayList<>(), user);
+        getFavoriteProducts();
+        searchedProducts.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        searchedProducts.setAdapter(productSearchedAdapter);
 
         getSearchedProducts(searchProd.getText().toString());
 
@@ -70,12 +76,30 @@ public class SearchedProductsFragment extends Fragment implements HeaderFragment
         }
     }
 
-    public void getSearchedProducts(String search) {
+    private void getFavoriteProducts() {
+        productManager.fetchUserFavOrCart(user.getUserId(), 1, new RepositoryCallback<List<Integer>>() {
+            @Override
+            public void onSuccess(List<Integer> favorites) {
+                favoriteIds.clear();
+                favoriteIds.addAll(favorites);
+
+                productSearchedAdapter.setFavoriteIds(favoriteIds);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e("API_RESPONSE", "Erro ao buscar produtos favoritos", t);
+            }
+        });
+    }
+
+    private void getSearchedProducts(String search) {
         productManager.searchProducts(search, new RepositoryCallback<List<Product>>() {
 
             @Override
             public void onSuccess(List<Product> result) {
-                productSearchedAdapter.updateProducts(result);
+                allProducts = result;
+                applyFilter();
             }
 
             @Override
@@ -83,9 +107,33 @@ public class SearchedProductsFragment extends Fragment implements HeaderFragment
         });
     }
 
+    private void applyFilter() {
+        List<Product> filtered = new ArrayList<>();
+
+        boolean onlyPoggers = poggersFilter.isChecked();
+
+        for (Product p : allProducts) {
+            if (onlyPoggers) {
+                if (p.isPoggers()) {
+                    filtered.add(p);
+                }
+            } else {
+                filtered.add(p);
+            }
+        }
+
+        productSearchedAdapter.updateProducts(filtered);
+    }
+
+
     private void initializeComponents(View view) {
         searchedProducts = view.findViewById(R.id.searchedProducts);
         searchProd = getActivity().findViewById(R.id.searchProd);
+        poggersFilter = view.findViewById(R.id.poggersFilter);
+
+        poggersFilter.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            applyFilter();
+        });
     }
 
     @Override
